ab78491046f8f5bc1ef43d61b451c4a7295c403a
Sanathsenthilkumar <166885449+Sanathsenthilkumar@users.noreply.github.com>
2025-01-30 16:05:50 +0530

    Add files via upload

---
+++ Javascript-in-JSX/App.jsx
@@ -0,0 +1,24 @@
+import DateDisplay from "./DisplayDate.jsx";
+import Quote from "./RandomQuote.jsx"
+import Add from "./Addition.jsx";
+import WordCount from "./WordCount.jsx";
+import Product from "./Multiplication.jsx"
+function App() {
+    return
+    //1)
+    //<DateDisplay />
+
+    //2
+    //<Quote />
+
+    //3
+    //<Add />
+
+    //4)
+    //<WordCount />
+
+    //5)
+    //<Product />
+}
+
+export default App
---
+++ Javascript-in-JSX/DisplayDate.jsx
@@ -0,0 +1,6 @@
+
+export default function DateDisplay(){
+    const d = new Date();
+    return d.toLocaleDateString(); // format date object to string
+
+}
