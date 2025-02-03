c5f6d285262d8d7542308773f1b52f5214cde94c
Sanathsenthilkumar <166885449+Sanathsenthilkumar@users.noreply.github.com>
2025-02-03 11:31:24 +0530

    Add files via upload

---
+++ Multi-component-file/App.jsx
@@ -0,0 +1,22 @@
+import UserName from "./DefaultExport/UserName.jsx";
+import UserAge from "./DefaultExport/UserAge.jsx";
+import UserComponent from "./UserComponent.jsx";
+export default function App() {
+  return//(
+    //1)
+    /*
+    <>
+      <UserName />
+      <UserAge />
+    </>
+    */
+   
+    //2) & 3) & 4) & 5)
+    /*
+    <>
+      <UserComponent.UserName />
+      <UserComponent.UserAge />
+    </>
+    */
+  //);
+}
---
+++ Multi-component-file/UserComponent.jsx
@@ -0,0 +1,15 @@
+function UserName(){
+    return(
+        <div>
+            <h1>Name: John doe</h1>
+        </div>
+    );
+}
+function UserAge(){
+    return(
+        <div>
+            <h1>Age: 21</h1>
+        </div>
+    )
+}
+export default {UserName, UserAge};
